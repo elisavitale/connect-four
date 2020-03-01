6fe54848d09b6f8ac761dc0cff423be5ed949a5c
elisavitale <elisavitale@live.it>
2020-03-01 21:52:16 +0100

    Created class Board + possible design of the game board.

---
+++ production/src/Board.java
@@ -0,0 +1,15 @@
+public class Board {
+
+    public static void printBoard() {
+        System.out.println(
+                " 1 2 3 4 5 6 7 \n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "| | | | | | | |\n" +
+                "---------------"
+        );
+    }
+}
