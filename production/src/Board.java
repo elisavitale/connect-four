28678bb1dd04e0b40a60de24e69219d7555a7047
elisavitale <elisavitale@live.it>
2020-08-26 14:42:43 +0200

    Boards can now be printed.

--- production/src/Board.java
+++ production/src/Board.java
@@ -1,15 +1,31 @@
 public class Board {
+    static String[][] board;
 
-    public static void printBoard() {
-        System.out.println(
-                " 1 2 3 4 5 6 7 \n" +
-                "| | | | | | | |\n" +
-                "| | | | | | | |\n" +
-                "| | | | | | | |\n" +
-                "| | | | | | | |\n" +
-                "| | | | | | | |\n" +
-                "| | | | | | | |\n" +
-                "---------------"
-        );
+    Board() {
+        board = new String[6][7];
+        initializeBoard();
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < 6; i++)
+            for (int j = 0; j < 7; j++)
+                board[i][j] = " ";
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 6; i++) {
+            printRow(i);
+        }
+    }
+
+    public static void printRow(int i) {
+        for (int j = 0; j < 7; j++)
+            System.out.print("|" + board[i][j]);
+        System.out.print("|\n");
+    }
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.printBoard();
     }
 }
