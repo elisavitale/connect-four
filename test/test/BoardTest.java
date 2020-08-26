6d801ebafe10cc4b63962c9f37925e044dfe25b8
elisavitale <elisavitale@live.it>
2020-08-26 14:40:52 +0200

    Testing printBoard() method.

---
+++ test/test/BoardTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+
+    @Test
+    void printBoardTest() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Board b = new Board();
+        b.printBoard();
+
+        String expectedOutput  = "| | | | | | | |\n" +
+                                 "| | | | | | | |\n" +
+                                 "| | | | | | | |\n" +
+                                 "| | | | | | | |\n" +
+                                 "| | | | | | | |\n" +
+                                 "| | | | | | | |\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
