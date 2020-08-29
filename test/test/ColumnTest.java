036fdf459cc4e6c7e461c4e2e35d6180c42845d3
elisavitale <elisavitale@live.it>
2020-08-29 17:31:49 +0200

    Testing the insertion of two pieces in the same column.

--- test/test/ColumnTest.java
+++ test/test/ColumnTest.java
@@ -9,4 +9,12 @@ public class ColumnTest {
         c.insert("R");
         assertEquals("R", c.array[5]);
     }
+
+    @Test
+    void insertTwiceTest() {
+        Column c = new Column(6);
+        c.insert("R");
+        c.insert("Y");
+        assertEquals("Y", c.array[4]);
+    }
 }
