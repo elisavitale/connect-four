b66db3ae7f50e89270390f973b329beed5cc7caa
elisavitale <elisavitale@live.it>
2020-09-01 17:16:19 +0200

    Testing horizontal alignments of four pieces.

--- test/test/BoardRulesTest.java
+++ test/test/BoardRulesTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BoardRulesTest {
+
+    Board b = new Board(6, 7);
+    BoardRules rules = new BoardRules(6, 7);
+
+    @Test
+    void connectFourHorizontalTest() {
+        b.insertPieceInColumn("Y", 1);
+        b.insertPieceInColumn("Y", 2);
+        b.insertPieceInColumn("Y", 3);
+        b.insertPieceInColumn("Y", 4);
+
+        assertTrue(rules.connectFourHorizontal());
+    }
+
+    @Test
+    void connectFourHorizontalTest2() {
+        b.insertPieceInColumn("Y", 1);
+        b.insertPieceInColumn("Y", 2);
+        b.insertPieceInColumn("Y", 3);
+        b.insertPieceInColumn("Y", 6);
+
+        assertFalse(rules.connectFourHorizontal());
+    }
+
+    @Test
+    void connectFourHorizontalTest3() {
+        b.insertPieceInColumn("Y", 1);
+        b.insertPieceInColumn("R", 2);
+        b.insertPieceInColumn("Y", 3);
+        b.insertPieceInColumn("Y", 4);
+        b.insertPieceInColumn("Y", 5);
+        b.insertPieceInColumn("Y", 6);
+
+        assertTrue(rules.connectFourHorizontal());
+    }
 }
