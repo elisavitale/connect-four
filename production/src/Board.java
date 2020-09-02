a6efbe1c5ee4fded42d88b59a70d1c7d247f6414
elisavitale <elisavitale@live.it>
2020-09-02 17:41:59 +0200

    Added method columnSizes()

--- production/src/Board.java
+++ production/src/Board.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
     int numberOfColumns;
@@ -18,32 +19,49 @@ public class Board {
     }
 
     public void printBoard() {
+        System.out.print(" 1 2 3 4 5 6 7 \n");
         for (int i = 1; i <= numberOfRows; i++)
             printRow(i);
     }
 
     public void printRow(int index) {
         ArrayList<String> row = getRow(index);
-        System.out.print("|");
-        System.out.print(String.join("|", row));
-        System.out.print("|\n");
+        System.out.print("|" + String.join("|", row) + "|\n");
     }
 
     public ArrayList<String> getRow(int index) {
         ArrayList<String> row = new ArrayList<>();
         for (int i = 0; i < numberOfColumns; i++)
-            row.add(board[i].getPiece(index));
+            row.add(board[i].getPieceAtRow(index));
         return row;
+    }
+
+    public ArrayList<String> getColumn(int index) {
+        ArrayList<String> column = new ArrayList<>();
+        for (int i = 1; i <= numberOfRows; i++)
+            column.add(board[index].getPieceAtRow(i));
+        return column;
+    }
+
+    public ArrayList<String> getDiagonal(int index) {
+        ArrayList<String> diagonal = new ArrayList<>();
+        int step = 6 - index;
+        for (int i = 0; i < numberOfColumns; i++) {
+            if (i + step == numberOfRows)
+                break;
+            diagonal.add(board[i].getPieceAtRow(i + step));
+        }
+        return diagonal;
     }
 
     public void insertPieceInColumn(String piece, int column) {
         board[column - 1].insert(piece);
     }
 
-    public ArrayList<String> getColumn(int index) {
-        ArrayList<String> column = new ArrayList<>();
-        for (int i = 1; i <= numberOfRows; i++)
-            column.add(board[index].getPiece(i));
-        return column;
+    public ArrayList<Integer> columnSizes() {
+        ArrayList<Integer> columnSizes = new ArrayList<Integer>();
+        for (int i = 0; i < numberOfColumns; i++)
+            columnSizes.add(board[i].currentSize());
+        return columnSizes;
     }
 }
