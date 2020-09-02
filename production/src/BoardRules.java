9b38f003a1e6e5f0102b70cc0db5109258c10788
elisavitale <elisavitale@live.it>
2020-09-02 10:43:19 +0200

    - The game can now detect when four pieces of the same color are connected horizontally - Fixed bug in the constructor BoardRules()

--- production/src/BoardRules.java
+++ production/src/BoardRules.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BoardRules {
     Board board;
 
-    BoardRules(int rows, int columns) {
-        board = new Board(rows, columns);
+    BoardRules(Board board) {
+        this.board = board;
     }
 
+    public boolean connectFourHorizontal() {
+        ArrayList<Boolean> rowValues = new ArrayList<>();
+        for (int i = 1; i <= board.numberOfRows; i++)
+            rowValues.add(checkRow(i));
+        return rowValues.contains(true);
+    }
+
+    public boolean checkRow(int index) {
+        ArrayList<String> row = board.getRow(index);
+        List<String> y = Arrays.asList("Y", "Y", "Y", "Y");
+        List<String> r = Arrays.asList("R", "R", "R", "R");
+        return Collections.indexOfSubList(row, y) != -1 || Collections.indexOfSubList(row, r) != -1;
+    }
 }
--- test/test/BoardRulesTest.java
+++ test/test/BoardRulesTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class BoardRulesTest {
 
     Board b = new Board(6, 7);
-    BoardRules rules = new BoardRules(6, 7);
+    BoardRules rules = new BoardRules(b);
 
     @Test
     void connectFourHorizontalTest() {
@@ -32,10 +32,10 @@ public class BoardRulesTest {
     void connectFourHorizontalTest3() {
         b.insertPieceInColumn("Y", 1);
         b.insertPieceInColumn("R", 2);
-        b.insertPieceInColumn("Y", 3);
         b.insertPieceInColumn("Y", 4);
         b.insertPieceInColumn("Y", 5);
         b.insertPieceInColumn("Y", 6);
+        b.insertPieceInColumn("Y", 7);
 
         assertTrue(rules.connectFourHorizontal());
     }
