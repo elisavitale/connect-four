624611c3da63574c8d33a883925d11ce0609175a
elisavitale <elisavitale@live.it>
2020-09-12 18:54:06 +0200

    Additions to class Game: - Added method setColor(); - Added method start().

--- production/src/Game.java
+++ production/src/Game.java
@@ -4,12 +4,36 @@ public class Game {
     Player player2;
     BoardRules rules;
 
-    Game() {
-        board = new Board(6, 7);
-        player1 = new Player(board, "R");
-        player2 = new Player(board, "Y");
+    Game(Board board) {
+        this.board = board;
+        System.out.print("PLAYER 1 >> ");
+        this.player1 = new Player(board);
+        System.out.print("PLAYER 2 >> ");
+        this.player2 = new Player(board, setColor());
+        System.out.println("Has color " + player2.color);
         rules = new BoardRules(board);
     }
 
-
+    private String setColor() {
+        if (player1.color.equals("R"))
+            return "Y";
+        else
+            return "R";
+    }
+    public void start() {
+        while (!board.isFull()) {
+            System.out.print("PLAYER 1 >> ");
+            int column1 = player1.chooseColumn();
+            board.insertPieceInColumn(player1.color, column1);
+            board.printBoard();
+            if (rules.connectFour(column1))
+                break;
+            System.out.print("PLAYER 2 >> ");
+            int column2 = player2.chooseColumn();
+            board.insertPieceInColumn(player2.color, column2);
+            board.printBoard();
+            if (rules.connectFour(column2))
+                break;
+        }
+    }
 }
