5853df46818c3fab15a080a50c45defecb0cd3dc
elisavitale <elisavitale@live.it>
2020-09-12 17:24:21 +0200

    Added constructor in class Game: handles initialization of board, players and game rules.

--- production/src/Game.java
+++ production/src/Game.java
@@ -2,10 +2,14 @@ public class Game {
     Board board;
     Player player1;
     Player player2;
+    BoardRules rules;
 
     Game() {
         board = new Board(6, 7);
-        player1 = new Player("R");
-        player2 = new Player("Y");
+        player1 = new Player(board, "R");
+        player2 = new Player(board, "Y");
+        rules = new BoardRules(board);
     }
+
+
 }
