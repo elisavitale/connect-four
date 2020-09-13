94e9aab137cfa279c447787dd723971e27c28251
elisavitale <elisavitale@live.it>
2020-09-13 08:44:50 +0200

    Additions to class Game: - fields player1, player2 are now replaced by HashMap<Integer, Player> players; - method setPlayers() handles the initialization of the players inside the constructor; - method start(), when called, starts the game. Player 1 makes the first move by default. The game stops either when the board is full or when a player makes a winning move.

--- production/src/Game.java
+++ production/src/Game.java
@@ -1,39 +1,68 @@
+import java.util.HashMap;
+
 public class Game {
     Board board;
-    Player player1;
-    Player player2;
-    BoardRules rules;
+    GameRules rules;
+    HashMap<Integer, Player> players = new HashMap<>();
 
     Game(Board board) {
         this.board = board;
-        System.out.print("PLAYER 1 >> ");
-        this.player1 = new Player(board);
-        System.out.print("PLAYER 2 >> ");
-        this.player2 = new Player(board, setColor());
-        System.out.println("Has color " + player2.color);
-        rules = new BoardRules(board);
+        setPlayers();
+        rules = new GameRules(board);
     }
 
-    private String setColor() {
+    private void setPlayers() {
+        System.out.print("PLAYER 1 >> ");
+        players.put(1, new Player(board));
+        System.out.print("PLAYER 2 >> ");
+        players.put(2, new Player(board, setPlayerColor()));
+        System.out.println("Has color " + getPlayer(2).color);
+    }
+
+    private String setPlayerColor() {
+        Player player1 = getPlayer(1);
         if (player1.color.equals("R"))
             return "Y";
-        else
-            return "R";
+        return "R";
     }
+
+    private Player getPlayer(int index) {
+        return players.get(index);
+    }
+
     public void start() {
+        int currentPlayerIndex = 1;
         while (!board.isFull()) {
-            System.out.print("PLAYER 1 >> ");
-            int column1 = player1.chooseColumn();
-            board.insertPieceInColumn(player1.color, column1);
-            board.printBoard();
-            if (rules.connectFour(column1))
+            System.out.print("PLAYER " + currentPlayerIndex + " >> ");
+            Player currentPlayer = getPlayer(currentPlayerIndex);
+            int column = move(currentPlayer);
+            if (winningMove(column)) {
+                gameOverMessage(currentPlayerIndex);
                 break;
-            System.out.print("PLAYER 2 >> ");
-            int column2 = player2.chooseColumn();
-            board.insertPieceInColumn(player2.color, column2);
-            board.printBoard();
-            if (rules.connectFour(column2))
-                break;
+            }
+            currentPlayerIndex = nextPlayer(currentPlayerIndex);
         }
+        System.out.println("Game over!");
+    }
+
+    private int move(Player player) {
+        int column = player.chooseColumn();
+        board.insertPieceInColumn(player.color, column);
+        board.printBoard();
+        return column;
+    }
+
+    private boolean winningMove(int column) {
+        return rules.connectFour(column);
+    }
+
+    private void gameOverMessage(int current) {
+        System.out.println("Player " + current + " wins the game.");
+    }
+
+    private int nextPlayer(int current) {
+        if (current == 1)
+            return 2;
+        return 1;
     }
 }
