821d3fe895c5af5b9379a082478a5b18dccd5eff
elisavitale <elisavitale@live.it>
2020-09-21 09:52:52 +0200

    Refactoring of class GameRules: - Removed lists fourR, fourY from method connectFour. They are now private fields of the class, as there is no need to compute fourR, fourY everytime the method connectFour is called. - Added method getCurrentRowIndexes(). - Renamed method currentRowIndex() to firstNonemptyRow().

--- production/src/GameRules.java
+++ production/src/GameRules.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GameRules {
     private Board board;
+    private List<String> fourR = alignmentOfLength("R", 4);
+    private List<String> fourY = alignmentOfLength("Y", 4);
 
     GameRules(Board board) {
         this.board = board;
@@ -11,36 +15,32 @@ public class GameRules {
 
     public boolean connectFour(int lastInput, boolean pop) {
         ArrayList<List<String>> rowColDiag = getCurrentRowColumnDiagonals(lastInput, pop);
-        List<String> fourR = alignmentOfLength("R", 4);
-        List<String> fourY = alignmentOfLength("Y", 4);
         return rowColDiag.stream()
                          .anyMatch(x -> containsAlignment(x, fourR) || containsAlignment(x, fourY));
     }
 
     private ArrayList<List<String>> getCurrentRowColumnDiagonals(int columnIndex, boolean pop) {
+        int[] currentRowIndexes = getCurrentRowIndexes(columnIndex, pop);
         ArrayList<List<String>> rowColDiag = new ArrayList<>();
-        if (pop) {
-            List<Integer> rowIndexes = new ArrayList<>();
-            for (int i = currentRowIndex(columnIndex); i <= board.numberOfRows; i++)
-                rowIndexes.add(i);
-            rowColDiag.add(board.getColumn(columnIndex));
-            for (int index : rowIndexes) {
-                rowColDiag.add(board.getRow(index));
-                rowColDiag.add(board.getDiagonal(index, columnIndex, false));
-                rowColDiag.add(board.getDiagonal(index, columnIndex, true));
-            }
-        }
-        else {
-            int rowIndex = currentRowIndex(columnIndex);
-            rowColDiag.add(board.getColumn(columnIndex));
-            rowColDiag.add(board.getRow(rowIndex));
-            rowColDiag.add(board.getDiagonal(rowIndex, columnIndex, false));
-            rowColDiag.add(board.getDiagonal(rowIndex, columnIndex, true));
-        }
+        rowColDiag.add(board.getColumn(columnIndex));
+        for (int rowIndex : currentRowIndexes)
+            rowColDiag.addAll(currentRowsAndDiagonals(rowIndex, columnIndex));
         return rowColDiag;
     }
 
-    private int currentRowIndex(int column) {
+    private List<List<String>> currentRowsAndDiagonals(int row, int column) {
+        return Arrays.asList(board.getRow(row),
+                             board.getDiagonal(row, column, false),
+                             board.getDiagonal(row, column, true));
+    }
+
+    private int[] getCurrentRowIndexes(int column, boolean pop) {
+        if (pop) return IntStream.range(firstNonemptyRow(column), board.numberOfRows + 1)
+                                 .toArray();
+        else return new int[] {firstNonemptyRow(column)};
+    }
+
+    private int firstNonemptyRow(int column) {
         return board.numberOfRows - board.currentSizeOfColumn(column) + 1;
     }
 
