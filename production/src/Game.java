e40dd499e22c47f0b5c346c78f59d74f25a9aee4
elisavitale <elisavitale@live.it>
2020-09-22 18:14:19 +0200

    Refactoring of class Game: - rearranged the body of method setPlayers(); - the value of boolean field popOut is now directly assigned by method setGameMode(); - adjusted method gameOver(); - added methods setInsertOrPop() and makeMoveOnBoard() to clean up the body of method move().

--- production/src/Game.java
+++ production/src/Game.java
@@ -12,15 +12,17 @@ public class Game {
         boardVisual = new BoardVisual(board);
         rules = new GameRules(board);
         setPlayers();
-        popOut = setGameMode();
+        setGameMode();
     }
 
     private void setPlayers() {
         System.out.print("\nPLAYER 1 >> ");
-        players.put(1, new Player(board));
+        Player player1 = new Player(board);
         System.out.print("\nPLAYER 2 >> ");
-        players.put(2, new Player(board, setPlayerColor()));
-        System.out.println("Has color " + getPlayer(2).color + ".\n");
+        Player player2 = new Player(board, setPlayerColor());
+        System.out.println("Has color " + player2.color + ".\n");
+        players.put(1, player1);
+        players.put(2, player2);
     }
 
     private String setPlayerColor() {
@@ -34,15 +36,15 @@ public class Game {
         return players.get(index);
     }
 
-    private boolean setGameMode() {
+    private void setGameMode() {
         Player player1 = players.get(1);
         String gameMode = player1.chooseGameMode();
-        return gameMode.equals("POP");
+        popOut = gameMode.equals("POP");
     }
 
     public void start() {
         int playerIndex = 1;
-        while (gameNotOver()) {
+        while (!gameOver()) {
             System.out.print("\nPLAYER " + playerIndex + " >> ");
             Player player = getPlayer(playerIndex);
             int column = move(player);
@@ -56,35 +58,39 @@ public class Game {
         System.out.println("Game over!");
     }
 
-    private boolean gameNotOver() {
-        if (popOut) return true;
-        return !board.isFull();
+    private boolean gameOver() {
+        if (popOut) return false;
+        return board.isFull();
     }
 
     private int move(Player player) {
-        int column;
-        if (popOut) {
-            String insertOrPop = player.chooseInsertOrPop();
-            column = player.chooseColumn(insertOrPop.equals("P"));
-            if (insertOrPop.equals("P")) {
-                board.popOut(column);
-            }
-            else
-                board.insertPieceInColumn(player.color, column);
-        }
-        else {
-            column = player.chooseColumn(false);
-            board.insertPieceInColumn(player.color, column);
-        }
+        boolean pop = setInsertOrPop(player, popOut);
+        int column = player.chooseColumn(pop);
+        makeMoveOnBoard(player.color, pop, column);
         return column;
     }
 
+    private boolean setInsertOrPop(Player player, boolean popOut) {
+        if (popOut) {
+            String insertOrPop = player.chooseInsertOrPop();
+            return insertOrPop.equals("P");
+        }
+        return false;
+    }
+
+    private void makeMoveOnBoard(String piece, boolean pop, int column) {
+        if (pop)
+            board.popOut(column);
+        else
+            board.insertPieceInColumn(piece, column);
+}
+
     private boolean winningMove(int column) {
-        return rules.connectFour(column, false);
+        return rules.connectFour(column, popOut);
     }
 
     private void winnerMessage(int current) {
-        System.out.println("Player " + current + " wins the game.");
+        System.out.println("\nPlayer " + current + " wins the game.");
     }
 
     private int nextPlayer(int current) {
